656b81a7f4e425f717d81d7840703ffc0251e4f0
Caio Nóbile <caio.nobile51@gmail.com>
2021-07-19 21:53:26 -0300

    Connecting to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,30 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import mongoose from "mongoose";
 dotenv.config();
 
 /**
  * App Variables
  */
-const PORT: number = parseInt(process.env.PORT as string, 10);
+const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
 const app = express();
+
+/**
+ * MongoDB Connection
+ */
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING as string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connection OK");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 /**
  *  App Configuration
