4d2b3c2969adb35c080da2adc43942a203391364
Caio Nóbile <caio.nobile51@gmail.com>
2021-07-21 00:55:21 -0300

    Finishing endpoints (w/o validations)

--- src/api/controllers/video.controller.ts
+++ src/api/controllers/video.controller.ts
@@ -1,7 +1,66 @@
 import video from "../models/video.schema";
 import { Request, Response } from "express";
 
-export const getVideos = async (req: Request, res: Response): Promise<Response> => {
-    const videos = await video.find()
-    return res.status(200).json(videos)
-}
+export const getVideos = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const videos = await video.find();
+  return res.status(200).json(videos);
+};
+
+export const getVideoById = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const videoSelected = await video.findById(req.params.id);
+  return res.status(200).json(videoSelected);
+};
+
+export const insertVideo = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const createdVideo = await video.create({ ...req.body });
+    return res.status(201).json(createdVideo);
+  } catch (e: any) {
+    return res.status(400).json({ error: e.message });
+  }
+};
+
+export const updateVideoById = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    await video.findByIdAndUpdate({ _id: req.params.id }, { ...req.body });
+    const videoUpdated = await video.findById(req.params.id);
+    return res.status(200).json(videoUpdated);
+  } catch (e: any) {
+    return res.status(404).json({ error: e.message });
+  }
+};
+
+export const replaceVideoById = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    await video.findOneAndReplace({ _id: req.params.id }, { ...req.body });
+    const videoUpdated = await video.findById(req.params.id);
+    return res.status(200).json(videoUpdated);
+  } catch (e: any) {
+    return res.status(404).json({ error: e.message });
+  }
+};
+
+export const deleteVideoById = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  if (await video.findById(req.params.id)) {
+    await video.findByIdAndRemove({ _id: req.params.id });
+    return res.status(200).json({ message: "Video deleted" });
+  } else return res.status(404).json({ message: "Video not found" });
+};
--- src/api/routes/video.ts
+++ src/api/routes/video.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
-/* import { getVideos } from "@controllers/video.controller"; */
-import { getVideos } from "../controllers/video.controller";
+import {
+  getVideos,
+  getVideoById,
+  insertVideo,
+  deleteVideoById,
+  updateVideoById,
+  replaceVideoById,
+} from "../controllers/video.controller";
 
 const router = Router();
 
-router.get("/", getVideos)
+router.post("/", insertVideo);
+router.get("/", getVideos);
+router.get("/:id", getVideoById);
+router.patch("/:id", updateVideoById);
+router.put("/:id", replaceVideoById);
+router.delete("/:id", deleteVideoById);
 
 export default router;
