8de26901aa53a2355573859417f22463668d8572
Caio Nóbile <caio.nobile51@gmail.com>
2021-07-22 01:13:36 -0300

    Adding URL validator

--- src/api/models/video.schema.ts
+++ src/api/models/video.schema.ts
@@ -3,7 +3,12 @@ import { Schema, model } from 'mongoose';
 const videoSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  url: { type: String, required: true },
+  url: {
+    type: String,
+    required: true,
+    match:
+      /(((ftp|http|https):\/\/)|(\/)|(..\/))(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/i,
+  },
 });
 
 export default model('Video', videoSchema);
