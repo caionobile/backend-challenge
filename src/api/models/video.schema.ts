d45cb6a9f9adf7b385f195cabc3b2a2fbeebe290
Caio Nóbile <caio.nobile51@gmail.com>
2021-07-22 23:19:31 -0300

    Creating variable

--- src/api/models/video.schema.ts
+++ src/api/models/video.schema.ts
@@ -1,13 +1,15 @@
 import { Schema, model } from 'mongoose';
 
+const urlRegex =
+  /(((ftp|http|https):\/\/)|(\/)|(..\/))(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/i;
+
 const videoSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   url: {
     type: String,
     required: true,
-    match:
-      /(((ftp|http|https):\/\/)|(\/)|(..\/))(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/i,
+    match: urlRegex,
   },
 });
 
